4652296e5c0773edce79f30dc237712f7b50148c
Rajesh Babu <rajesh.babu@oviva.com>
2023-03-15 15:55:53 +0100

    CHORE: Adding read-times and fixing the headings

--- app/routes/_mdx.tsx
+++ app/routes/_mdx.tsx
@@ -1,17 +1,44 @@
 import styles from "highlight.js/styles/github-dark-dimmed.css";
-import type { LinksFunction } from "@remix-run/node";
-import { Outlet } from "@remix-run/react";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node";
+import { Outlet, useLoaderData, useLocation } from "@remix-run/react";
+import type { BlogList } from "~/data/blogList.server";
+import { blogList } from "~/data/blogList.server";
+
+export const loader: LoaderFunction = async () => {
+  return blogList;
+};
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
 };
 
 export default function BlogLayout() {
+  const { pathname } = useLocation();
+  const posts = useLoaderData<BlogList[]>();
+
+  const currentPost = posts.find(({ pathName }) => pathName === pathname);
+
   return (
-    <div className="flex justify-center">
-      <div className="prose w-screen py-10 px-10 dark:prose-invert md:prose-lg lg:prose-xl prose-headings:text-text-primary dark:prose-headings:text-d-text-primary">
-        <Outlet />
+    <>
+      {currentPost ? (
+        <header className="text-center">
+          <p className="my-10 font-sans font-semibold uppercase text-text-secondary dark:text-d-text-secondary">
+            {currentPost.publishDate} ~ {currentPost.readingTime} read
+          </p>
+          <h1 className="mb-20 text-4xl font-bold leading-[1.3] md:text-6xl">
+            {currentPost.title}
+          </h1>
+          <hr className="w-[30%] min-w-[100px]" />
+        </header>
+      ) : (
+        <></>
+      )}
+
+      <div className="flex justify-center">
+        <div className="prose w-screen py-10 px-10 dark:prose-invert md:prose-lg lg:prose-xl prose-headings:text-text-primary prose-a:no-underline prose-pre:p-0 dark:prose-headings:text-d-text-primary">
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
