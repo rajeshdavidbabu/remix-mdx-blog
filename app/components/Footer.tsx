11d27d4528d7d70282d970ca98eab6b98a509d5c
Rajesh Babu <rajesh.babu@oviva.com>
2023-03-16 12:37:02 +0100

    CHORE: Adding some fine refinements

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,25 @@ export const Footer = () => {
   return (
     <footer className="mx-auto my-4 w-full max-w-7xl px-8 py-4 text-center text-[0.8rem] text-text-secondary dark:text-d-text-secondary">
       <span>
-        &copy; {new Date().getFullYear()} Rajesh Babu. Powered by{" "}
+        &copy; {new Date().getFullYear()} Rajesh Babu. Find me on{" "}
+        <a
+          href="https://www.linkedin.com/in/rajesh-babu-56270046/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin
+        </a>{" "}
+        or{" "}
+        <a
+          href="https://twitter.com/razzmattazzz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+        .
+        <br />
+        Powered by{" "}
         <a href="https://remix.run/" target="_blank" rel="noopener noreferrer">
           Remix
         </a>
