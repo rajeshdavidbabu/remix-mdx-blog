0ba102b49fb2f1ca8f08e9d6f41097e215bce3f9
Rajesh Babu <rajesh.babu@oviva.com>
2023-03-16 11:39:41 +0100

    CHORE: Header style using css

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 export const Header = () => {
   return (
-    <header className="sticky top-[0px] z-[1] mx-auto flex w-full max-w-7xl flex-wrap items-center justify-between bg-inherit p-[2em] font-sans font-bold uppercase text-text-primary backdrop-blur-[100px] dark:text-d-text-primary">
+    <header className="app-header mx-auto flex w-full max-w-7xl flex-wrap items-center justify-between p-[2em] font-sans font-bold uppercase text-text-primary dark:text-d-text-primary">
       <Logo />
       <Nav />
     </header>
