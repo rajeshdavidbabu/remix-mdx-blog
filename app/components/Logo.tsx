317dc1423b37873d7c66d0215b7a4f27efa1d98e
Rajesh Babu <rajesh.babu@oviva.com>
2023-03-22 18:44:37 +0100

    CHORE: Add animations to logo

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,12 +1,17 @@
+import { motion, isTouch } from "framer-motion";
+
 export const Logo = () => {
   return (
     <a href="/" className="inline-block">
-      <img
+      <motion.img
         alt="Blog Logo"
         src="/assets/images/logo.webp"
         className="block w-[75px]"
         width="75"
         height="50"
+        animate={{ rotate: 360 }}
+        transition={{ type: "spring", stiffness: 100, damping: 20 }}
+        whileHover={{ rotate: 180 }}
       />
     </a>
   );
